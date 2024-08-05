2c2c8c28f468e83709c8f5722fa56738659dfc40
Aron Ciruela <ciruela.aron@gmail.com>
2024-08-05 21:37:53 +0800

    Implement DELETE Endpoint

--- src/main/java/com/ciruela/cashcard/CashCardRepository.java
+++ src/main/java/com/ciruela/cashcard/CashCardRepository.java
@@ -12,5 +12,7 @@ interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSo
 	CashCard findByIdAndOwner(Long id, String owner);
 	
 	Page<CashCard> findByOwner(String owner, PageRequest pageRequest);
+	
+	boolean existsByIdAndOwner(Long id, String name);
 
 }
